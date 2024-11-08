d3f651e7390b4c9019c3d2166d12e5c7609ccd1f
LinuxFredur <fredd3_95@hotmail.com>
2024-11-08 22:58:33 +0100

    right before adding init swapchain code.

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -2,6 +2,7 @@
 
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_vulkan.h>
+#include <VkBootstrap.h>
 
 //#include <vk_initializers.h>
 #include "vk_types.h"
@@ -14,6 +15,7 @@
 //FASTGLTF SAKNAR parser.hpp, kolla när aktuellt
 #include <fastgltf/glm_element_traits.hpp>
 
+//Decide if validation layers should be used.
 constexpr bool bUseValidationLayers = false;
 
 //Initialize pointer to the engine instance. For the VulkanEngine::Get() function.
@@ -43,6 +45,11 @@ void VulkanEngine::init()
         m_windowExtent.width,
         m_windowExtent.height,
         window_flags);
+
+    init_vulkan();
+	init_swapchain();
+	init_commands();
+	init_sync_structures();
 
     // everything went fine
     m_isInitialized = true;
@@ -98,6 +105,70 @@ void VulkanEngine::run()
 }
 
 void VulkanEngine::draw()
+{
+
+}
+
+void VulkanEngine::init_vulkan()
+{
+    // ~~~~~~~~~~Initialize VkInstance~~~~~~~~~~~~~~
+    //Use vk-bootstrap to initialize the VkInstance.
+    vkb::InstanceBuilder builder;
+
+    //make the vulkan instance, with basic debug features
+	auto inst_ret = builder.set_app_name("Vulkanutbrott")
+		.request_validation_layers(bUseValidationLayers)
+		.use_default_debug_messenger()
+		.require_api_version(1, 3, 0)
+		.build();
+
+	vkb::Instance vkb_inst = inst_ret.value();
+
+	//Set the member variables VkInstance and VkDebugUtilsMessengerEXT
+	m_instance = vkb_inst.instance;
+	m_debug_messenger = vkb_inst.debug_messenger;
+
+    //~~~~~~~~~~~Initialize VkDevice, choose VkPhysicalDevice (GPU) and create abstract surface VkSurfaceKHR.
+    SDL_Vulkan_CreateSurface(m_window_ptr, m_instance, NULL, &m_surface); //The NULL argument is a VKAllocatorCallback struct, unclear what part it plays. 
+
+    //vulkan 1.3 features, Dynamic rendering and synchronization2
+	VkPhysicalDeviceVulkan13Features features{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+	features.dynamicRendering = true;
+	features.synchronization2 = true;
+
+	//vulkan 1.2 features
+	VkPhysicalDeviceVulkan12Features features12{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+	features12.bufferDeviceAddress = true; //Lets us use GPU pointers without binding buffers
+	features12.descriptorIndexing = true; //Gives us bindless textures
+
+    //use vkbootstrap to select a gpu. 
+	//We want a gpu that can write to the SDL surface and supports vulkan 1.3 with the correct features (Selected above. Dynamic rendering etc.)
+	vkb::PhysicalDeviceSelector selector{ vkb_inst };
+	vkb::PhysicalDevice physicalDevice = selector
+		.set_minimum_version(1, 3)
+		.set_required_features_13(features)
+		.set_required_features_12(features12)
+		.set_surface(m_surface)
+		.select()
+		.value();
+
+	//create the final vulkan device
+	vkb::DeviceBuilder deviceBuilder{ physicalDevice };
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	// Get the VkDevice handle used in the rest of a vulkan application
+	m_device = vkbDevice.device;
+	m_physicalGPU = physicalDevice.physical_device;
+}
+void VulkanEngine::init_swapchain()
+{
+
+}
+void VulkanEngine::init_commands()
+{
+
+}
+void VulkanEngine::init_sync_structures()
 {
 
 }
--- src/vk_engine.h
+++ src/vk_engine.h
@@ -3,26 +3,34 @@
 #include "vk_types.h"
 
 class VulkanEngine {
-public:
 
+public:
 	bool m_isInitialized{ false }; //Flag to know if engine is initialized.
 	int m_frameNumber {0}; //Frame number, can render one frame and work on another for instance.
-	bool stop_rendering{ false }; //Boolean to stop rendering when window minimized for example.
-	VkExtent2D m_windowExtent{ 1700 , 900 }; //Window size
+	bool stop_rendering{ false }; //Boolean to stop rendering when window minimized for example
 
+	VkExtent2D m_windowExtent{ 1700 , 900 }; //Window size
 	struct SDL_Window* m_window_ptr{ nullptr }; //Forward declare to have pointer to window without including SDL in the engine header.
 
+	//These variables are required to initialize Vulkan
+	VkInstance m_instance; //All information of the vulkan state is stored in the instance. Initializes the Vulkan library.
+	VkSurfaceKHR m_surface;//VkSurfaceKHR is an abstract window that we can present images to, to be delivered to the windowing system. Should be created right after the instance.
+	VkDebugUtilsMessengerEXT m_debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice m_physicalGPU; //A reference to the GPU and information of it is stored in this handle. Properties such as VRAM can be queried from it.
+	VkDevice m_device; //The VkDevice is the driver of the VkPhysicalDevice that we use to communicate with the GPU.
+	
 	static VulkanEngine& Get(); //Get() function as global singleton pattern. Returns reference to the engine instance.
+	void init();	//initializes everything in the engine
+	void cleanup();	//shuts down the engine
+	void draw();	//draw loop
+	void run();	//run main loop
 
-	//initializes everything in the engine
-	void init();
+	private:
+	//These functions will be contained in the main init() function, and will initialize the parts.
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
 
-	//shuts down the engine
-	void cleanup();
 
-	//draw loop
-	void draw();
-
-	//run main loop
-	void run();
 };
